b337ee6970275d0da77ccc5204db8405c7bc36fd
grace-ko <gracekoder@gmail.com>
2017-02-25 21:23:55 -0800

    readme added, indentation fixed

---
+++ README.md
@@ -0,0 +1,2 @@
+#FizzBuzz Refractor
+Allows user to pick a number from 1-100 to which traditional fizzbuzz is applied 
--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 var userAnswer = prompt("Pick a number from 1-100!");
-var result= function(){
-	var stringToNum=parseInt(userAnswer);
-	for (var i=1; i<=stringToNum; i++){
-		if (i%3==0 && i%5==0) {
-			$('.result').append("fizzubuzz"+"<br>");
-		}else if (i%3==0) {
-			$('.result').append('fizz'+"<br>");
-		}else if (i%5==0) {
-			$('.result').append('buzz'+'<br>');
-		}else {
-			$('.result').append(i+"<br>");
-		}
-	}	
+var result = function() {
+    var stringToNum = parseInt(userAnswer);
+    for (var i = 1; i <= stringToNum; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            $('.result').append("fizzubuzz" + "<br>");
+        } else if (i % 3 == 0) {
+            $('.result').append('fizz' + "<br>");
+        } else if (i % 5 == 0) {
+            $('.result').append('buzz' + '<br>');
+        } else {
+            $('.result').append(i + "<br>");
+        }
+    }
 };
 
 result();
